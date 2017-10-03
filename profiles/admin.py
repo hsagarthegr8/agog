260fb70ab58c3586cf7a2ad805b2f295ab322318
= <=>
2017-10-03 12:27:46 +0530

    Added profile model and avatars

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import UserRating, Connections
+from .models import UserRating, Connections, Profile
 
 admin.site.register(UserRating)
 admin.site.register(Connections)
+admin.site.register(Profile)
--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,20 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    image = models.ImageField(blank=True)
+    lives_in = models.CharField(max_length=120,blank=True)
+    hometown = models.CharField(max_length=120,blank=True)
+    RELATIONSHIP = [('S','Single'),('I','In a Relationship')]
+    relationship = models.CharField(max_length=1, choices=RELATIONSHIP,blank=True)
+    interests = models.TextField(max_length=300,blank=True)
+    about = models.TextField(max_length=500,blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
 class UserRating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rated_by_set')
