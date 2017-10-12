e5cee0a38619b43e78a6371f04b07601732798a3
= <=>
2017-10-12 12:20:21 +0530

    Port to bootstrap 4

--- agog/views.py
+++ agog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from accounts.forms import UserCreationForm
+from django.shortcuts import redirect,reverse
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -7,3 +8,9 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         kwargs['form']=UserCreationForm()
         return super(HomeView, self).get_context_data(**kwargs)
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(reverse('profiles:timeline'))
+        return super(HomeView, self).get(request,*args,**kwargs)
+
