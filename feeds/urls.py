ba31b973da11d5c7512bb3d3aaf12b9687d625e9
= <=>
2017-10-17 13:39:54 +0530

    Added post and comment settings

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,11 +1,14 @@
 
 from django.conf.urls import url
 
-from .views import add_post,add_comment
+from .views import add_post, add_comment, delete_post, delete_comment
 
 app_name = 'feeds'
 
 urlpatterns = [
     url(r'^post/(?P<username>[a-z,0-9]+)/new/$', add_post, name='add_post'),
-    url(r'^comment/(?P<username>[a-z,0-9]+)/new/$', add_comment, name='add_comment')
+    url(r'^comment/new/$', add_comment, name='add_comment'),
+    url(r'^post-delete/$', delete_post, name='delete_post'),
+    url(r'^comment-delete/$', delete_comment, name='delete_comment'),
+
 ]
--- feeds/views.py
+++ feeds/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, reverse
+from django.shortcuts import redirect, reverse,get_object_or_404
+from django.http import HttpResponseRedirect
 from django.contrib.auth import get_user_model
 
 from .models import Post, Comment
@@ -10,19 +11,31 @@ def add_post(request,username):
     posted_by = request.user
     posted_on = User.objects.get(username=username)
     message = request.POST['message']
-    redirect_to = request.POST['redirect_to']
     Post.objects.create(posted_by=posted_by, posted_on=posted_on, message=message)
-    if redirect_to == 'profile':
-        return redirect(reverse('profiles:profile', kwargs={'username': posted_on.username}))
-    return redirect(reverse('profiles:timeline'))
+    next = request.POST.get('next', '/')
+    return HttpResponseRedirect(next)
 
 
-def add_comment(request, username):
+def delete_post(request):
+    post_id = int(request.POST['post'])
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    next = request.POST.get('next', '/')
+    return HttpResponseRedirect(next)
+
+
+def add_comment(request):
     post_id = int(request.POST['post'])
     post = Post.objects.get(id=post_id)
     comment = request.POST['comment']
     Comment.objects.create(commented_by=request.user,post=post,comment=comment)
-    if request.POST['redirect_to'] == 'profile':
-        return redirect(reverse('profiles:profile', kwargs={'username': username}))
-    return redirect(reverse('profiles:timeline'))
+    next = request.POST.get('next', '/')
+    return HttpResponseRedirect(next)
 
+
+def delete_comment(request):
+    comment_id = int(request.POST['comment'])
+    comment = Comment.objects.get(id=comment_id)
+    comment.delete()
+    next = request.POST.get('next', '/')
+    return HttpResponseRedirect(next)
